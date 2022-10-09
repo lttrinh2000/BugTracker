abe9e892af783d40eb871ca811653352401f7ed6
lttrinh2000 <trinhtrilong@yahoo.com>
2022-10-09 15:05:08 -0500

    Hide AWS keys.

--- src/main/java/com/longtrinh/profileimage/ServerConfig/AwsConfig.java
+++ src/main/java/com/longtrinh/profileimage/ServerConfig/AwsConfig.java
@@ -1,5 +1,9 @@
 package com.longtrinh.profileimage.ServerConfig;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,11 +20,10 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 public class AwsConfig {
     
     @Bean
-    public AmazonS3 s3() {
-        AWSCredentials credentials = new BasicAWSCredentials(
-            "AKIA2SAJQXZWQOBHHIMB", 
-            "bTXOYeUQkU8Q2ghe06QKpjhQCHosfrNzgMMr8bR7"
-        );
+    public AmazonS3 s3() throws IOException {
+        String accessKey = Files.readAllLines(Paths.get("../rootkey.csv")).get(0);
+        String secretKey = Files.readAllLines(Paths.get("../rootkey.csv")).get(1);
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 
         return AmazonS3ClientBuilder
                 .standard()
